76c9768e884fb5bba0a8a175e8a677596affe664
KocsisAGyorgy <gurikocsis31@gmail.com>
2024-04-24 10:37:40 +0200

    handled location, pokemon randomized

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import PokemonEncounter from './components/PokemonEncounter';
 
 function App() {
   const [pageState, setPageState] = useState('welcome');
 
   return (
     <div>
-
+      <PokemonEncounter
+      url = "https://pokeapi.co/api/v2/location/2/"/>
     </div>
   )
 }
 
-export default App
+export default App;
