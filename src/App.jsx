a09df67f90948f8dac990d80a97d8f270537ecc8
Mate <matet2001@gmail.com>
2024-04-24 11:14:24 +0200

    Finish attack function

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Battle from './Components/Battle.jsx'
 import './Styles/App.css'
 
 function App() {
-  const [pageState, setPageState] = useState('welcome');
-
   return (
     <div>
       <Battle ownPokemon='https://pokeapi.co/api/v2/pokemon/pikachu' opponentPokemon='https://pokeapi.co/api/v2/pokemon/ditto' />
--- src/Components/Battle.jsx
+++ src/Components/Battle.jsx
@@ -3,17 +3,31 @@ import {fetchJsonData, getRandomArbitrary} from '../utility.js'
 import { PokemonCard } from './PokemonCard.jsx';
 import '../Styles/Battle.css'
 
-function Attack(attacker, defender){
-    const B = attacker.stats[1].base_stat; //attacker attack
-    const D = defender.stats[2].base_stat; //defender defense
+function ProcessBattle(ownPokemon, ownSetter, opponentPokemon, opponentSetter, setPageState){ 
+    Attack(ownPokemon, opponentPokemon, opponentSetter);
+    if(opponentPokemon.hp <= 0){
+        console.log(ownPokemon.name, 'wins!')
+        setPageState('locations')
+    }
+    Attack(opponentPokemon, ownPokemon, ownSetter);
+    if(ownPokemon.hp <= 0){
+        console.log(opponentPokemon.name, 'wins!')
+        setPageState('locations')
+    }
+}
+
+function Attack(attacker, defender, defenderSetter){
+    const B = attacker.attack; //attacker attack
+    const D = defender.defend; //defender defense
     const Z = getRandomArbitrary(217, 255);
 
     const damage = ((((2/5+2)*B*60/D)/50)+2)*Z/255;
-    defender[0].base_stat -= damage;
+    const newHp = defender.hp - Math.floor( damage);
+    defenderSetter(pokemon => ({...pokemon, hp: newHp}));
 }
 
-function ProcessBattle(firstPokemon, secondPokemon){
-    
+function isDead(pokemon){
+    return pokemon.stats[0].base_stat <= 0;
 }
 
 function Battle(props){
@@ -21,23 +35,42 @@ function Battle(props){
     const [opponentPokemon, setOpponentPokemon] = useState(null);
 
     useEffect(() => {
+        async function getPokemon(pokemonUrl){
+            const pokemon = await fetchJsonData(pokemonUrl);
+            return {
+                name: pokemon.name,
+                hp: pokemon.stats[0].base_stat,
+                attack: pokemon.stats[1].base_stat,
+                defend: pokemon.stats[2].base_stat,
+                speed:  pokemon.stats[5].base_stat,
+                sprite: pokemon.sprites.other.home.front_default
+            };
+        }
+
         async function getPokemons(){
-            const _ownPokemon = await fetchJsonData(props.ownPokemon);
-            setOwnPokemon(_ownPokemon);
-            setOpponentPokemon(await fetchJsonData(props.opponentPokemon));
+            const _ownPokemon = await getPokemon(props.ownPokemon);
+            const _opponentPokemon = await getPokemon(props.opponentPokemon);
+
+            _ownPokemon['attacker'] = _ownPokemon.speed > _opponentPokemon.speed;
+            _opponentPokemon['attacker'] = _opponentPokemon.speed > _ownPokemon.speed;
+
+            setOwnPokemon(_ownPokemon);       
+            setOpponentPokemon(_opponentPokemon);
         }
 
         getPokemons();
     }, []);
 
+    
+
     return (
         <>
             <h1>BATTLE</h1>
             <div className='cardContainer'>
-                {ownPokemon ? (<PokemonCard name={ownPokemon.name} hp={ownPokemon.stats[0].base_stat} image={ownPokemon.sprites.other.home.front_default}/>) : <p>Loading...</p>}
-                {opponentPokemon ? (<PokemonCard name={opponentPokemon.name} hp={opponentPokemon.stats[0].base_stat} image={opponentPokemon.sprites.other.home.front_default}/>) : <p>Loading...</p>}
+                {ownPokemon ? (<PokemonCard name={ownPokemon.name} hp={ownPokemon.hp} image={ownPokemon.sprite}/>) : <p>Loading...</p>}
+                {opponentPokemon ? (<PokemonCard name={opponentPokemon.name} hp={opponentPokemon.hp} image={opponentPokemon.sprite}/>) : <p>Loading...</p>}
             </div>
-            <button>Begin!</button>
+            <button onClick={() => ProcessBattle(ownPokemon, setOwnPokemon, opponentPokemon, setOpponentPokemon, props.setStage)}>Attack!</button>
         </>
     );
 }
