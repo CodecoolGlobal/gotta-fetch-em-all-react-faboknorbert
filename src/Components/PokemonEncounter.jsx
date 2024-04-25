1f45be0f829a893d821929f47c0f13bce544a606
KocsisAGyorgy <gurikocsis31@gmail.com>
2024-04-25 11:33:15 +0200

    prep-work for developemnt

--- src/Components/PokemonEncounter.jsx
+++ src/Components/PokemonEncounter.jsx
@@ -34,6 +34,7 @@ function PokemonEncounter ({ userData, setStage, setBattleData, }) {
         
         async function pickRandomPokemon(url) {
             const pokemonList = await collectPokemons(url)
+            if (pokemonList.length <= 0) setStage("Locations")
             const sortedList = [... new Set(pokemonList)];
             const randomPokemon = sortedList[Math.floor(Math.random() * sortedList.length)];
             setPokemonURL(randomPokemon);
